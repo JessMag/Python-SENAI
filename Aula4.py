50eb723b6d2860792d8e1ca475b1a4f5fa46f958
JessMag <jessica.s.magalhaes99@gmail.com>
2025-07-25 21:38:57 -0300

    Add files via upload

---
+++ Aula4.py
@@ -0,0 +1,24 @@
+#1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+#n1 = int(input('digite um numero: '))
+#print('O quadadro de',n1,'é', n1**2)
+
+
+#2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+#nome = input('digite seu nome: ')
+#sobrenome = input('digite seu sobrenome: ')
+#print('seu nome completo é: ', nome, sobrenome)
+
+#3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+#n2 = int(input('digite um numero: '))
+#n3 = int(input('digite um numero: '))
+#print(str(n2),str(n3))
+
+#4 - Crie uma variável para armazenar a palavra "Python". Em segui
+#py = 'python'
+#n4 = int(input('digite um numero: '))
+#print(py,n4)
+
+#5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado.
+var = 'curso python 60 h'
+palavra = input ('digite uma palavra: ')
+print(var,palavra)
