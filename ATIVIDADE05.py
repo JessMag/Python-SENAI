dfddbb5333129b51a254b51488082fd9f021c842
JessMag <jessica.s.magalhaes99@gmail.com>
2025-07-29 21:33:21 -0300

    Add files via upload

---
+++ ATIVIDADE05.py
@@ -0,0 +1,59 @@
+#Multiplique 4 por 6 e divida o resultado por 2. (Concatene com a palavra resultado)
+multiplicação = (4*6)*2
+print('resultado multiplicação é',multiplicação)
+
+#Calcule a soma de 5 e 7.(Concatene com a palavra resultado)
+soma = 5+7
+print('resultado soma é',soma)
+
+#Eleve 3 à potência de 4.(Concatene com a palavra resultado)
+potencia = 3**4
+print('resultado potência é',potencia)
+
+#Calcule a média de 8, 12 e 15.(Concatene com a palavra resultado)
+media = (8+12+15)/3
+print('resultado média é',round(media,2))
+
+#Subtraia 10 de 2 e multiplique o resultado por 5.(Concatene com a palavra resultado)
+subtracao = (2-10)*5
+print('resultado subtração é',subtracao)
+
+#Divida 27 por 3 e some 5.(Concatene com a palavra resultado)
+divisao = (27/3)+5
+print ('resultado divisao é',divisao)
+
+#Calcule 9o módulo ( % ) de 17 por 4.Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+modulo = (((17%4)*9)*3)**2
+print (modulo)
+
+#Calcule a raiz quadrada de 81.
+raiz = 81**0.5
+print (raiz)
+
+#Adicione 20 a 3 vezes 4.
+add = (3*4)+20
+print (add)
+
+#Multiplique 15 por 2 e, em seguida, subtraia 7.
+multi = ((15*2)-7)
+print(multi)
+
+#Eleve 5 ao cubo
+potencia2 = 5**3
+print(potencia2)
+
+#Calcule a média de 17, 21 e 25.
+media2 = (17+21+25)/3
+print(media2)
+
+#Multiplique 11 por 2 e adicione 7
+multi2 = (11*2)+7
+print(multi2)
+
+#Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+sub2 = ((3*8)-15)/2
+print(sub2)
+
+#Eleve 2 à potência de 10.
+potencia3 = 2**10
+print(potencia3)
