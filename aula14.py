0fdc44605d0b0600c5c6087c06c35b2e6e000b1b
JessMag <jessica.s.magalhaes99@gmail.com>
2025-08-12 21:54:47 -0300

    Add files via upload

---
+++ aula14.py
@@ -0,0 +1,31 @@
+#EXERCÍCIOS:**
+#Utilizando Python:
+
+import os
+import shutil
+
+
+#Exercício 1: Criar e ler um Arquivo**
+
+# Criar um arquivo  "with"
+open('arquivo3', 'w')
+
+
+#Exemplo 2: Cria um Diretório**
+#os.mkdir('novo2')
+
+#Exercício 3: Renomear um Diretório**
+#os.rename('arquivo', 'novo_nome2.txt')
+
+#xercício 4:  Listar Arquivos em um Diretório** 
+#with os.scandir('novo') as entrada:
+#    for arquivo in entrada:
+#        if arquivo.is_file():
+#            print(f'Arquivo encontrado: {arquivo.name}')
+
+#Exercício 5:  Copiar Arquivos em um Diretório**
+#shutil.copytree('novo', 'novo nome')
+
+
+#Exercício 6:  Remover*
+#shutil.rmtree('C:/Users/Aluno/Downloads/Nova pasta/novo nome')
